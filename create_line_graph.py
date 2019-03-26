1460288967ceaf165c76bc4e30b1af18e31aca71
Jo11jo <35219360+Jo11jo@users.noreply.github.com>
2019-03-26 20:34:59 +0100

    Update create_line_graph.py

--- create_line_graph.py
+++ create_line_graph.py
@@ -1,23 +1,36 @@
 import matplotlib.pyplot as plt
 
-def create_line_graph(cbsdata):
-    years = []
-    for i in range(44):
-        years.append(int(cbsdata.iloc[i,1]))
-
-    country_data = cbsdata.loc[0:43,"Nationaliteit":"TotaalAsielverzoekenEnNareizigers_1"]
-
-    y_values = []
-
-    for i in range(44):
-        try:
-            y_values.append(int(country_data.iloc[i,2]))
-        except:
-            y_values.append(0)
-
-    line_graph = plt.plot(years,y_values,label="Total")
+def create_line_graph(cbsdata,start_year,end_year,countries):
+    
+    # Dictionary that relates country to country code
+    country_dict = {'Total Requests':'T001059','Syria':'NAT9444','Iran':'NAT9351','China':'NAT9310'}
+    
+    for country in countries:     
+        # Creates list of years and a copy
+        years = []
+        trimmed_years = []
+        i = int(start_year)
+        while i <= int(end_year):
+            years.append(i)
+            trimmed_years.append(i)
+            i = i + 1     
+               
+        # Trims the dataframe down to only the required country
+        country_data = cbsdata[cbsdata['Nationaliteit']==country_dict[country]]
+    
+        # Creates a list of asylum requests per year for that country. If there is
+        # no data available, the data point is removed. 
+        asylum_requests = []
+        for i in years:
+            try:
+                asylum_requests.append(int(country_data[country_data['Perioden']==str(i)].iloc[0,2]))
+            except:
+                trimmed_years.remove(i)        
+    
+        # Plot the asylum requests over time for the country
+        plt.plot(trimmed_years,asylum_requests,label=country)
+    
     plt.xlabel('Years')
     plt.ylabel('Asylum Requests')
-    plt.title('Asylum Requests for the Netherlands')
+    plt.title('Asylum Requests to the Netherlands')
     plt.legend()
-    return line_graph
