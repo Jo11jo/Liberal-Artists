1b6d6ccf73331c0a9514afff0bbc82e7c9ae927c
Karelzz <48929390+Karelzz@users.noreply.github.com>
2019-03-27 17:42:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,64 @@ while True:
     5\tExit program.
     """)
     
-    if choice == "1":
+    if choice == "1":     
         countries = [] 
+        all_countries = ['Total Requests','Syria','Iran','China','Turkey','Afghanistan','Irak','Eritrea','Sudan','Bosnia and Herzegovina','Romania']
+        all_countries.sort()
         while True:
             print("Which countries would you like to view? Please choose one from this list:")
-            print("'Total Requests','Syria','Iran','China'")
-            countries.append(input())
+            for country in all_countries:
+                print(country)
+            while True:
+                country_choice = input()
+                if country_choice in all_countries:
+                    if country_choice not in countries:
+                        countries.append(country_choice)
+                        break
+                    else:
+                        print("That country is already in the list.")
+                else:
+                    print("You did not enter a country from the list, please try again.")
+            print("\n")
             print("Current countries:")
-            print(countries)
-            choice = input("Would you like to add another country? (y/n) ")
-            if choice == 'n':
+            for country in countries:
+                print(country)
+            countries.sort()
+            if countries != all_countries:
+                choice = input("Would you like to add another country? (y/n) ")
+                print("\n")
+                if choice == 'n':
+                    break
+            else:
                 break
         
-        print("What time period would you like to view? Please enter a start and end year.")
-        start_year = input("Start year: ")
-        end_year = input("End year: ")
+        while True:
+            print("What time period would you like to view? Please enter a start and end year (between 1975 and 2018).")
+            while True:
+                try:
+                    start_year = int(input("Start year: "))
+                    break
+                except:
+                    print("You did not enter an integer!")
+            if start_year < 1975 or start_year > 2017:
+                print("You entered an invalid start year, please try again.")
+                print("\n")
+                continue
+            while True:
+                try:
+                    end_year = int(input("End year: "))
+                    break
+                except:
+                    print("You did not enter an integer!")     
+            if end_year < 1975 or end_year > 2018:
+                print("You entered an invalid end year, please try again.")
+                print("\n")
+                continue
+            if end_year <= start_year:
+                print("The end year must be after the start year!")
+                print("\n")
+                continue
+            break            
         
         graph = lg.create_line_graph(cbsdata,start_year,end_year,countries)
         plt.show()
