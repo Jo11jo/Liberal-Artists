b22f3c6cb16d7899da2a70c573ecd19edc500e63
Jo11jo <35219360+Jo11jo@users.noreply.github.com>
2019-03-28 16:55:13 +0100

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ while True:
     1\tView asylum requests (per country of origin) to the Netherlands over time.
     2\tView asylum requests (per country of destination) over time.
     3\tView migrating population demographics (to NL) per country of origin (gender, age, etc.).
-    4\tView migration flows to the Netherlands in a map for specific year.
-    5\tExit program.
+    4\tExit program.
     """)
     
     if choice == "1":
@@ -149,8 +148,6 @@ while True:
     elif choice == "3":
         print("To do: Create clustered bar chart.")
     elif choice == "4":
-        print("To do: Create map.")
-    elif choice == "5":
         print("Thank you for using this program.")
         break
     else:
