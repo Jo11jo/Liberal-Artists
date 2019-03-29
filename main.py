45dca8b2973e7638e14357bfddd1774c1792ae91
Jo11jo <35219360+Jo11jo@users.noreply.github.com>
2019-03-29 15:13:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ all_countries = ['Total Requests','Syria','Iran','China','Turkey',\
                          'Afghanistan','Irak','Eritrea','Sudan','Bosnia and Herzegovina','Romania']
 all_countries.sort()
 
+
 while True:  
     choice = input("""Welcome! What do you want to do?
     1\tView asylum requests (per country of origin) to the Netherlands over time.
@@ -80,6 +81,10 @@ while True:
         graph = lg.line_graph_NL(cbsdata_NL,start_year,end_year,countries)
         plt.show()
         
+        
+        
+        
+        
     elif choice == "2":
         
         countries = [] 
@@ -144,9 +149,108 @@ while True:
         
         graph = lg.line_graph_int(cbsdata_int,start_year,end_year,countries)
         plt.show()     
-                                
+          
+          
+          
+          
     elif choice == "3":
-        print("To do: Create clustered bar chart.")
+        # list of countries that will later be handed into the function defining desired countries to compare
+        countries = [] 
+        # all countries that are possible to compare
+        all_countries = ['Total Requests','Syria','Iran','China','Turkey',\
+                                 'Afghanistan','Irak','Eritrea','Sudan','Bosnia and Herzegovina','Romania']
+        all_countries.sort()
+
+        while True:
+            #ask user input which countries
+            print("Which countries would you like to compare? Please choose one from this list:")
+            for country in all_countries:
+                print(country)
+            while True:
+                country_choice = input()
+                if country_choice in all_countries:
+                    if country_choice not in countries:
+                        # if country possible to choose and hasn't been chosen yet
+                        countries.append(country_choice)
+                        break
+                    else:
+                        print("That country is already in the list. Please enter another country.")
+                else:
+                    print("You did not enter a country from the list, please try again.")
+            print("\n")
+            #display chosen countries, add more? ask user input
+            print("Current countries:")
+            for country in countries:
+                print(country)
+            countries.sort()
+            if countries != all_countries:
+                choice = input("Would you like to add another country? (y/n) ")
+                print("\n")
+                if choice == 'n':
+                    # break if no oher country should be chosen
+                    break
+                elif choice != 'y' and choice != 'n':
+                    print("This answer can't be recognized. Enter another country or just hit enter.")
+            else:
+                # break if all countries possible have been chosen
+                break
+
+        #repeat process with possibilities of demographics to compare across countries
+        groups = []
+        all_groups = ['gender', 'age', 'marital status']
+        while True:
+            print("These are the demographics you can choose to compare:")
+            for group in all_groups:
+                print(group)
+            while True:
+                choice = input("Please indicate one demographic you want to compare across countries \n")
+                if choice in all_groups:
+                    if choice not in groups:
+                        groups.append(choice)
+                        break
+                    else:
+                        print("That demographic is already being compared. Please enter another one.")
+                else:
+                    print("You did not enter a demographic from the list, please try again.")
+            print("\n")
+            print("Current demographics:")
+            for group in groups:
+                print(group)
+            groups.sort()
+            if groups != all_groups:
+                choice = input("Would you like to add another demographic? (y/n) ")
+                print("\n")
+                if choice == 'n':
+                    break
+                elif choice != 'y' and choice != 'n':
+                    print("This answer can't be recognized. Enter another country or just hit enter.")
+            else:
+                break
+
+
+        # plot graph for each demographics to compare
+        if 'gender' in groups:
+            #get according dataset
+            gender_df = dh.load_data_gender('data_gender.csv')
+            #plt graph along the list of countries chosen
+            bg.create_bargraph_demographics(gender_df, countries)
+            plt.show()
+
+        if 'age' in groups:
+            age_df = dh.load_data_age('data_age.csv')
+            bg.create_bargraph_demographics(age_df, countries)
+            plt.show()
+
+        if 'marital status' in groups:
+            mar_stat_df = dh.load_data_mar_status('data_mar_status.csv')
+            bg.create_bargraph_demographics(mar_stat_df, countries)
+            plt.show()
+
+       
+        
+        
+        
+        
     elif choice == "4":
         print("Thank you for using this program.")
         break
